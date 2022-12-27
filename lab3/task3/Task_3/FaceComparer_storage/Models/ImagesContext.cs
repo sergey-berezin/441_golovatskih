0c79840c7e2387fe8d89aec933d663eef93cd42c
mglvc <m.golovatskikh@gmail.com>
2022-12-28 02:15:21 +0300

    lab 3

---
+++ lab3/task3/Task_3/FaceComparer_storage/Models/ImagesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FaceComparer_storage.Models
+{
+    public class ImagesContext : DbContext
+    {
+        public DbSet<ImageItem> Images { get; set; }
+        public DbSet<ImageDetails> ImagesDetails { get; set; }
+
+        public ImagesContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Data Source=Database.db");
+        }
+    }
+}
